357e99a737dd198c8dde1bc8e224f85a882bfc99
ngahirinuki <hir23003@byui.edu>
2023-10-04 23:19:12 +1300

    week2 update

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,12 +3,9 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
 const fullName = "Nga Hirinuki";
 const currentYear = new Date().getFullYear();
-
-const profilePicture = "images/nga.jpg";
-
+const profilePicture = "images/nga.png";
 
 
 
@@ -21,13 +18,31 @@ imgElement.id = 'profile';
 const imageElement = document.getElementById('profile');
 
 /* Step 4 - Adding Content */
-
-nameElement.innerHTML = `<strong> ${fullName} </strong>`;
-
-imageElement.setAttribute("src", profilePicture);
-yearElement.textContent = `${currentYear}`;
+nameElement.innerHTML = `<strong> ${fullName} <strong>`;
+imageElement.src = profilePicture;
 
 /* Step 5 - Array */
 
-let favFoods = ["Pork Roast","Chop-suey","Potato Salad","Taro","Kumara"];
+let favFoods = ["Pork Roast "," Chop-suey "," Potato Salad ","Taro "," Kumara"];
 foodElement.textContent = favFoods;
+
+foodElement.innerHTML += '<br>';
+favFoods.push(', Banana');
+
+for (let i = 0; i < favFoods.length; i++) {
+foodElement.innerHTML += `${favFoods[i]}`;
+}
+
+foodElement.innerHTML += '<br>';
+favFoods.shift();
+
+for (let i = 0; i < favFoods.length; i++) {
+foodElement.innerHTML += `${favFoods[i]}`;
+}
+
+foodElement.innerHTML += '<br>';
+favFoods.pop();
+
+for (let i = 0; i < favFoods.length; i++) {
+foodElement.innerHTML += `${favFoods[i]}`;
+}
